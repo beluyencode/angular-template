af271d86ef9f0779d68482c27ed404038388ba0c
beluyencode <longlc956@gmail.com>
2022-12-13 17:15:18 +0700

    init

---
+++ src/app/create-template/create-template.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CreateTemplateComponent } from './create-template.component';
+
+
+
+@NgModule({
+  declarations: [
+    CreateTemplateComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CreateTemplateComponent
+  ]
+})
+export class CreateTemplateModule { }
