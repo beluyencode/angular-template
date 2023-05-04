0af8d25c9f31ae15d2eaf9fbb21f12d9f654cad0
beluyencode <longlc956@gmail.com>
2023-05-04 23:44:02 +0700

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  data = [1, 2]
-  title = 'angular-template';
-  arrGrammar: any = null;
+
   constructor() { }
 
   ngOnInit(): void {
